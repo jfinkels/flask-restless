0dc8fb527b78d52f6ec1f0d5ce4f3bd64ec61dbe
Lincoln de Sousa <lincoln@comum.org>
2011-04-11 19:33:17 -0300

    Adding README, setup.py and tests/__init__.py files

---
+++ setup.py
@@ -0,0 +1,29 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='RESTfulEf',
+    version='0.1',
+    author='Lincoln de Sousa',
+    author_email='lincoln@comum.org',
+    description='A generic restful api generator based on elixir and flask',
+    license='AGPLv3+',
+    keywords='rest api flask elixir',
+    url='http://projects.comum.org/restful',
+    packages=('restful', 'tests'),
+    long_description=read('README'),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Topic :: Database',
+        'Topic :: Database :: Front-Ends',
+        'Topic :: Internet :: WWW/HTTP',
+    ]
+)
