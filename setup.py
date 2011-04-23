ec87b570a2f7d10096221f7941f791c17d3d9b30
Lincoln de Sousa <lincoln@comum.org>
2011-04-23 01:19:15 -0300

    bump to version 0.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
 
 setup(
     name='RESTfulEf',
-    version='0.1.1',
+    version='0.2',
     author='Lincoln de Sousa',
     author_email='lincoln@comum.org',
     description='A generic restful api generator based on elixir and flask',
