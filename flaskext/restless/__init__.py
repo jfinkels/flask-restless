ff45f2a2f3b41591c9e185654f32c5d850911a90
Jeffrey Finkelstein <jeffrey.finkelstein@gmail.com>
2012-02-29 01:11:02 -0500

    Expose APIManager and Entity as part of the Python public API.

--- flaskext/restless/__init__.py
+++ flaskext/restless/__init__.py
@@ -1,1 +1,26 @@
+# -*- coding: utf-8; Mode: Python -*-
+#
+# Copyright 2012 Jeffrey Finkelstein <jeffrey.finkelstein@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 __version__ = '0.3-dev'
+"""The current version of this extension.
+
+This should be the same as the version specified in the :file:`setup.py` file.
+
+"""
+
+# make the following names available as part of the public API
+from api import APIManager
+from model import Entity
