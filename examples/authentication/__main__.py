72ba63dd51b6f40ba1a1666e5e99d83de429a194
Jeffrey Finkelstein <jeffrey.finkelstein@gmail.com>
2012-04-02 13:55:57 -0400

    Updated example to work with SQLAlchemy.

--- examples/authentication/__main__.py
+++ examples/authentication/__main__.py
@@ -25,15 +25,15 @@
 
     This requires the following Python libraries to be installed:
 
-    * Elixir
     * Flask
     * Flask-Restless
     * Flask-Login
     * Flask-WTF
+    * SQLAlchemy
 
     To install them using ``pip``, do::
 
-        pip install Elixir Flask Flask-Restless Flask-Login Flask-WTF
+        pip install Flask Flask-Restless Flask-Login Flask-WTF SQLAlchemy
 
     To use this example, run this package from the command-line. If you are
     using Python 2.7 or later::
@@ -58,30 +58,38 @@
 import os
 import os.path
 
-from elixir import metadata, setup_all, create_all, session, Field, Unicode
 from flask import Flask, render_template, redirect, url_for
-from flask.ext.restless import APIManager, Entity
+from flask.ext.restless import APIManager
 from flask.ext.login import current_user, login_user, LoginManager, UserMixin
 from flask.ext.wtf import PasswordField, SubmitField, TextField, Form
+from sqlalchemy import create_engine, Column, Integer, Unicode
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# Create the declarative base class for the SQLAlchemy models.
+Base = declarative_base()
 
 
 # Step 1: create the user database model.
-class User(Entity, UserMixin):
-    username = Field(Unicode)
-    password = Field(Unicode)
+class User(Base, UserMixin):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    username = Column(Unicode)
+    password = Column(Unicode)
 
-# Step 2: setup the database.
+# Step 2: setup the database and the SQLAlchemy session.
 DATABASE = os.path.join(os.path.dirname(os.path.abspath(__file__)),
                         'test.sqlite')
 if os.path.exists(DATABASE):
     os.unlink(DATABASE)
-metadata.bind = 'sqlite:///{}'.format(DATABASE)
-metadata.bind.echo = False
-setup_all()
-create_all()
+engine = create_engine('sqlite:///%s' % DATABASE)
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
 
 # Step 3: create a test user in the database.
 user1 = User(username=u'example', password=u'example')
+session.add(user1)
 session.commit()
 
 # Step 4: create the Flask application and its login manager.
@@ -93,24 +101,18 @@ login_manager.setup_app(app)
 # Step 5: this is required for Flask-Login.
 @login_manager.user_loader
 def load_user(userid):
-    return User.get(userid)
+    return session.query(User).filter_by(id=userid).first()
 
 
-# Step 6: create the login and add user forms.
+# Step 6: create the login form.
 class LoginForm(Form):
     username = TextField('username')
     password = PasswordField('password')
     submit = SubmitField('Login')
 
 
-class AddForm(Form):
-    username = TextField('username')
-    password = PasswordField('password')
-    submit = SubmitField('Login')
-
-
 # Step 7: create endpoints for the application, one for index and one for login
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
@@ -123,7 +125,8 @@ def login():
         # you would check username and password here...
         #
         username, password = form.username.data, form.password.data
-        user = User.get_by(username=username, password=password)
+        user = session.query(User).filter_by(username=username,
+                                             password=password).one()
         login_user(user)
         return redirect(url_for('index'))
     return render_template('login.html', form=form)
@@ -131,10 +134,11 @@ def login():
 # Step 8: create the API for User.
 api_manager = APIManager(app)
 auth_func = lambda: current_user.is_authenticated()
-api_manager.create_api(User, authentication_required_for=['GET'],
+api_manager.create_api(session, User, authentication_required_for=['GET'],
                        authentication_function=auth_func)
 
 # Step 9: configure and run the application
 app.config['DEBUG'] = True
+app.config['TESTING'] = True
 app.config['SECRET_KEY'] = os.urandom(24)
 app.run()
