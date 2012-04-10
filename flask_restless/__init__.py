80e95021ebf7b85e3a31c363d04cdd374903175c
Jeffrey Finkelstein <jeffrey.finkelstein@gmail.com>
2012-04-10 19:13:06 -0400

    Prepare for next development cycle (for version 0.6-dev).

--- flask_restless/__init__.py
+++ flask_restless/__init__.py
@@ -16,7 +16,7 @@
 #:
 #: This should be the same as the version specified in the :file:`setup.py`
 #: file.
-__version__ = '0.5'
+__version__ = '0.6-dev'
 
 # make the following name available as part of the public API
 from .manager import APIManager
--- setup.py
+++ setup.py
@@ -97,6 +97,6 @@ setup(
     test_suite='tests.suite',
     tests_require=['unittest2'],
     url='http://github.com/jfinkels/flask-restless',
-    version='0.5',
+    version='0.6-dev',
     zip_safe=False
 )
