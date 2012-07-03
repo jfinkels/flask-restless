82b1d7c1e7dc1488a8c15b460ce32c88e41b5491
Jeffrey Finkelstein <jeffrey.finkelstein@gmail.com>
2012-07-03 15:49:03 -0400

    Added automatic git tag message to release script.

--- scripts/make-release.py
+++ scripts/make-release.py
@@ -135,7 +135,8 @@ def make_git_commit(message, *args):
 
 def make_git_tag(tag):
     info('Tagging "%s"', tag)
-    Popen(['git', 'tag', '-s', tag]).wait()
+    msg = '"Released version {}"'.format(tag)
+    Popen(['git', 'tag', '-s', '-m', msg, tag]).wait()
 
 
 def main():
