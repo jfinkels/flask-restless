3f0d1b2ac6d08f517b6a492e6da74cbc4a70db47
Jeffrey Finkelstein <jeffrey.finkelstein@gmail.com>
2013-01-17 16:46:16 -0500

    Bump version number to 0.9.1

--- flask_restless/__init__.py
+++ flask_restless/__init__.py
@@ -16,7 +16,7 @@
 #:
 #: This should be the same as the version specified in the :file:`setup.py`
 #: file.
-__version__ = '0.9.1-dev'
+__version__ = '0.9.1'
 
 # make the following name available as part of the public API
 from .manager import APIManager
