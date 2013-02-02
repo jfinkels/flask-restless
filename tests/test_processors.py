d11256181c7607a959cd4fcb4be6f0c14907bd2c
Jeffrey Finkelstein <jeffrey.finkelstein@gmail.com>
2013-02-02 17:48:19 -0500

    Cleans up test code.

--- tests/test_processors.py
+++ tests/test_processors.py
@@ -1,17 +1,18 @@
 """
     tests.test_processors
-    ~~~~~~~~~~~~~~~~
+    ~~~~~~~~~~~~~~~~~~~~~
 
     Provides unit tests for pre- and post-processors hooks.
 
-    :copyright: 2011 by Lincoln de Sousa <lincoln@comum.org>
-    :copyright: 2012 Jeffrey Finkelstein <jeffrey.finkelstein@gmail.com>
+    :copyright: 2013 "klinkin" <klinkin@gmail.com>
+    :copyright: 2013 Jeffrey Finkelstein <jeffrey.finkelstein@gmail.com>
     :license: GNU AGPLv3+ or BSD
 
 """
 from __future__ import with_statement
 
 from datetime import date
+from unittest2 import TestSuite
 
 from flask import json
 from flask.ext.restless.views import StopPreprocessor, StopPostprocessor
@@ -23,8 +24,8 @@ dumps = json.dumps
 loads = json.loads
 
 
-class ProcessorsTestCase(TestSupport):
-    """Unit tests for the :class:`flask_restless.views.API` class."""
+class ProcessorsTest(TestSupport):
+    """Unit tests for preprocessors and postprocessors."""
 
     def setUp(self):
         """Creates the database, the :class:`~flask.Flask` object, the
@@ -32,90 +33,80 @@ class ProcessorsTestCase(TestSupport):
 
         """
         # create the database
-        super(ProcessorsTestCase, self).setUp()
+        super(ProcessorsTest, self).setUp()
 
         # to facilitate searching
         self.app.search = lambda url, q: self.app.get(url + '?q=%s' % q)
 
     def test_get_single_preprocessor(self):
-        """Tests GET method for single object with preprocessors function."""
+        """Tests :http:method:`get` requests for a single object with
+        a preprocessor function.
+
+        """
 
         def check_permissions(instid):
-
-            current_user_have_permission_to_read_obj = False
-            person = self.session.query(self.Person).filter_by(id=instid).first()
-
-            # check permission current user for obj inst
-            if person and not current_user_have_permission_to_read_obj:
-                raise StopPreprocessor(status_code=403, message='Permission denied')
+            raise StopPreprocessor(status_code=403,
+                                   message='Permission denied')
 
         pre = dict(GET_SINGLE=[check_permissions])
-        # create the api
         self.manager.create_api(self.Person, methods=['GET', 'POST'],
-                                url_prefix='/api/v1',
                                 preprocessors=pre)
-
-        # Creating people
-        self.app.post('/api/v1/person',
-                      data=dumps({'name': u'Lincoln', 'age': 23}))
-
-        response = self.app.get('/api/v1/person/1')
+        response = self.app.post('/api/person', data=dumps({'name': u'test'}))
+        self.assertEqual(201, response.status_code)
+        response = self.app.get('/api/person/1')
         self.assertEqual(response.status_code, 403)
 
     def test_get_list_preprocessor(self):
-
         def check_permissions(params):
-            current_user_have_permission_to_read_only_objs = [1, 3]
+            filt = {u'name': u'id', u'op': u'in', u'val': [1, 3]}
             if 'filters' not in params:
-                params['filters'] = [{u'name': u'id', u'op': u'in', u'val': current_user_have_permission_to_read_only_objs}]
+                params['filters'] = [filt]
             else:
-                params['filters'].append({u'name': u'id', u'op': u'in', u'val': current_user_have_permission_to_read_only_objs})
+                params['filters'].append(filt)
             return params
 
         pre = dict(GET_LIST=[check_permissions])
-        # create the api at /api/v1/person
         self.manager.create_api(self.Person, methods=['GET', 'POST'],
-                                url_prefix='/api/v1',
                                 preprocessors=pre)
 
-        # Creating some people
-        self.app.post('/api/v1/person',
+        self.app.post('/api/person',
                       data=dumps({'name': u'Lincoln', 'age': 23}))
-        self.app.post('/api/v1/person',
+        self.app.post('/api/person',
                       data=dumps({'name': u'Lucy', 'age': 23}))
-        self.app.post('/api/v1/person',
+        self.app.post('/api/person',
                       data=dumps({'name': u'Mary', 'age': 25}))
 
-        response = self.app.get('/api/v1/person')
+        response = self.app.get('/api/person')
         objs = loads(response.data)['objects']
         ids = [obj['id'] for obj in objs]
         self.assertEqual(ids, [1, 3])
         self.assertEqual(response.status_code, 200)
 
-        search = {
-            'filters': [
-                {'name': 'name', 'val': u'Lincoln', 'op': 'equals'}
-            ],
-        }
-        response = self.app.search('/api/v1/person', dumps(search))
+        search = dict(filters=[dict(name='name', val='Lincoln', op='equals')])
+        response = self.app.search('/api/person', dumps(search))
         num_results = loads(response.data)['num_results']
 
         self.assertEqual(num_results, 1)
         self.assertEqual(response.status_code, 200)
 
     def test_post_preprocessor(self):
-        """Tests POST method decoration using a custom function."""
-        def decorator_function(params):
+        """Tests :http:method:`post` requests with a preprocessor function."""
+        def add_parameter(params):
             if params:
                 # just add a new attribute
                 params['other'] = 7
             return params
 
-        pre = dict(POST=[decorator_function])
-        # test for function that decorates parameters with 'other' attribute
+        def check_permissions(params):
+            raise StopPreprocessor(status_code=403,
+                                   message='Permission denied')
+
         self.manager.create_api(self.Person, methods=['POST'],
                                 url_prefix='/api/v2',
-                                preprocessors=pre)
+                                preprocessors=dict(POST=[add_parameter]))
+        self.manager.create_api(self.Person, methods=['POST'],
+                                url_prefix='/api/v3',
+                                preprocessors=dict(POST=[check_permissions]))
 
         response = self.app.post('/api/v2/person',
                                  data=dumps({'name': u'Lincoln', 'age': 23}))
@@ -125,153 +116,122 @@ class ProcessorsTestCase(TestSupport):
         person = self.session.query(self.Person).filter_by(id=personid).first()
         self.assertEquals(person.other, 7)
 
-    def test_post_preprocessor_permission(self):
-        """Tests POST method decoration using a custom function."""
-
-        def check_permissions(params):
-            current_user_have_permission_to_create_obj = False
-            # check permission current user for obj inst
-            if current_user_have_permission_to_create_obj:
-                return params
-            else:
-                raise StopPreprocessor(status_code=403, message='Permission denied')
-
-        pre = dict(POST=[check_permissions])
-        self.manager.create_api(self.Person, methods=['POST'],
-                                url_prefix='/api/v2',
-                                preprocessors=pre)
-
-        response = self.app.post('/api/v2/person',
+        response = self.app.post('/api/v3/person',
                                  data=dumps({'name': u'Lincoln', 'age': 23}))
         self.assertEqual(response.status_code, 403)
 
     def test_delete_preprocessor(self):
+        """Tests for using a preprocessor with :http:method:`delete` requests.
 
+        """
         def check_permissions(instid):
-            current_user_have_permission_to_delete_obj = False
-            person = self.session.query(self.Person).filter_by(id=instid).first()
-
-            # check permission current user for obj inst
-            if person and not current_user_have_permission_to_delete_obj:
-                raise StopPreprocessor(status_code=403, message='Permission denied')
+            raise StopPreprocessor(status_code=403,
+                                   message='Permission denied')
 
         pre = dict(DELETE=[check_permissions])
         # recreate the api at /api/v1/person
         self.manager.create_api(self.Person,
-                                url_prefix='/api/v1',
-                                methods=['GET', 'PATCH', 'POST', 'DELETE'],
+                                methods=['POST', 'DELETE'],
                                 preprocessors=pre)
 
         # Creating some people
-        self.app.post('/api/v1/person',
+        self.app.post('/api/person',
                       data=dumps({'name': u'Lincoln', 'age': 23}))
-        self.app.post('/api/v1/person',
+        self.app.post('/api/person',
                       data=dumps({'name': u'Lucy', 'age': 23}))
-        self.app.post('/api/v1/person',
+        self.app.post('/api/person',
                       data=dumps({'name': u'Mary', 'age': 25}))
 
         # Try deleting it
-        response = self.app.delete('/api/v1/person/1')
+        response = self.app.delete('/api/person/1')
         self.assertEqual(response.status_code, 403)
 
         # Making sure it has been not deleted
         people = self.session.query(self.Person).filter_by(id=1)
         self.assertEquals(people.count(), 1)
 
-
     def test_patch_single_preprocessor(self):
+        """Tests for using a preprocessor with :http:method:`patch` requests.
+
+        """
 
         def check_permissions(instid, data):
-            current_user_have_permission_to_update_obj = False
-            person = self.session.query(self.Person).filter_by(id=instid).first()
-
-            # check permission current user for obj inst
-            if person and not current_user_have_permission_to_update_obj:
-                raise StopPreprocessor(status_code=403, message='Permission denied')
+            raise StopPreprocessor(status_code=403,
+                                   message='Permission denied')
 
         pre = dict(PATCH_SINGLE=[check_permissions])
         # recreate the api at /api/v1/person
         self.manager.create_api(self.Person,
-                                url_prefix='/api/v1',
-                                methods=['GET', 'PATCH', 'POST', 'DELETE'],
+                                methods=['PATCH', 'POST'],
                                 preprocessors=pre)
 
         # Creating some test people
-        self.app.post('/api/v1/person',
+        self.app.post('/api/person',
                       data=dumps({'name': u'Lincoln', 'age': 23}))
-        self.app.post('/api/v1/person',
+        self.app.post('/api/person',
                       data=dumps({'name': u'Lucy', 'age': 23}))
-        self.app.post('/api/v1/person',
+        self.app.post('/api/person',
                       data=dumps({'name': u'Mary', 'age': 25}))
 
         # Try updating people with id=1
-        response = self.app.patch('/api/v1/person/1', data=dumps({'age': 27}))
+        response = self.app.patch('/api/person/1', data=dumps({'age': 27}))
         self.assertEqual(response.status_code, 403)
 
 
     def test_patch_single_preprocessor2(self):
+        """Tests for using a preprocessor with :http:method:`patch` requests.
 
-        def check_permissions_and_update_data(instid, data):
-            current_user_have_permission_to_update_obj = True
-            person = self.session.query(self.Person).filter_by(id=instid).first()
+        """
 
-            # check permission current user for obj inst
-            if person and not current_user_have_permission_to_update_obj:
-                raise StopPreprocessor(status_code=403, message='Permission denied')
-
+        def update_data(instid, data):
             data['other'] = 27
             return data
 
-        pre = dict(PATCH_SINGLE=[check_permissions_and_update_data])
+        pre = dict(PATCH_SINGLE=[update_data])
         # recreate the api at /api/v1/person
         self.manager.create_api(self.Person,
-                                url_prefix='/api/v1',
-                                methods=['GET', 'PATCH', 'POST', 'DELETE'],
+                                methods=['GET', 'PATCH', 'POST'],
                                 preprocessors=pre)
 
         # Creating some test people
-        self.app.post('/api/v1/person',
+        self.app.post('/api/person',
                       data=dumps({'name': u'Lincoln', 'age': 23}))
-        self.app.post('/api/v1/person',
+        self.app.post('/api/person',
                       data=dumps({'name': u'Lucy', 'age': 23}))
-        self.app.post('/api/v1/person',
+        self.app.post('/api/person',
                       data=dumps({'name': u'Mary', 'age': 25}))
 
         # Try updating people with id=1
-        response = self.app.patch('/api/v1/person/1', data=dumps({'age': 27}))
+        response = self.app.patch('/api/person/1', data=dumps({'age': 27}))
         self.assertEqual(response.status_code, 200)
 
-        resp = self.app.get('/api/v1/person/1')
+        resp = self.app.get('/api/person/1')
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(loads(resp.data)['age'], 27)
         self.assertEqual(loads(resp.data)['other'], 27)
 
-
     def test_patch_many_preprocessor(self):
+        """Tests for using a preprocessor with :http:method:`patch` requests
+        which request changes to many objects.
+
+        """
 
         def check_permissions_and_update_data(params):
-            current_user_have_permission_to_update_all_obj = True
-
-            # check permission current user for obj inst
-            if current_user_have_permission_to_update_all_obj:
-                params['other'] = 27
-                return params
-            else:
-                raise StopPreprocessor(status_code=403, message='Permission denied')
+            params['other'] = 27
+            return params
 
         pre = dict(PATCH_MANY=[check_permissions_and_update_data])
         # recreate the api at /api/v1/person
         self.manager.create_api(self.Person, methods=['GET', 'POST', 'PATCH'],
-                                url_prefix='/api/v1',
                                 allow_patch_many=True,
                                 preprocessors=pre)
 
         # Creating some people
-        self.app.post('/api/v1/person',
+        self.app.post('/api/person',
                       data=dumps({'name': u'Lincoln', 'age': 23}))
-        self.app.post('/api/v1/person',
+        self.app.post('/api/person',
                       data=dumps({'name': u'Lucy', 'age': 23}))
-        self.app.post('/api/v1/person',
+        self.app.post('/api/person',
                       data=dumps({'name': u'Mary', 'age': 25}))
 
 
@@ -279,13 +239,19 @@ class ProcessorsTestCase(TestSupport):
         day, month, year = 15, 9, 1986
         birth_date = date(year, month, day).strftime('%d/%m/%Y')  # iso8601
         form = {'birth_date': birth_date}
-        response = self.app.patch('/api/v1/person', data=dumps(form))
-
+        response = self.app.patch('/api/person', data=dumps(form))
 
         # Finally, testing if the change was made
-        response = self.app.get('/api/v1/person')
+        response = self.app.get('/api/person')
         loaded = loads(response.data)['objects']
         for i in loaded:
             self.assertEqual(i['birth_date'], ('%s-%s-%s' % (
                     year, str(month).zfill(2), str(day).zfill(2))))
             self.assertEqual(i['other'], 27)
+
+
+def load_tests(loader, standard_tests, pattern):
+    """Returns the test suite for this module."""
+    suite = TestSuite()
+    suite.addTest(loader.loadTestsFromTestCase(ProcessorsTest))
+    return suite
