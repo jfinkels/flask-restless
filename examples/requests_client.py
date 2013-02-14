12ae8a9c610e7ef4db240e1f398d95ed2b4f4b8c
Jeffrey Finkelstein <jeffrey.finkelstein@gmail.com>
2013-02-13 23:59:28 -0500

    Adds an example for using the requests library as a client.

---
+++ examples/requests_client.py
@@ -0,0 +1,49 @@
+"""
+    Using Flask-Restless with the "requests" library
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    This provides an example of using Flask-Restless on the server side to
+    provide a ReSTful API and the Python `requests <>`_ library on the client
+    side to make HTTP requests to the server.
+
+    To install the requests library::
+
+        pip install requests
+
+    Before executing the code in this module, you must first run the quickstart
+    server example from this directory (that is, the ``examples/`` directory)::
+
+        PYTHONPATH=.. python quickstart.py
+
+    Now run this script from this directory to see some example requests using
+    the ``requests`` library::
+
+        python requests_client.py
+
+    Remember, the client must specify the ``application/json`` MIME type when
+    sending requests.
+
+    :copyright: 2013 Jeffrey Finkelstein <jeffrey.finkelstein@gmail.com>
+    :license: GNU AGPLv3+ or BSD
+
+"""
+import json
+import requests
+
+url = 'http://127.0.0.1:5000/api/person'
+headers = {'Content-Type': 'application/json'}
+
+# Make a POST request to create an object in the database.
+data = dict(name='Jeffrey', birth_date='1-2-1923')
+response = requests.post(url, data=json.dumps(data), headers=headers)
+assert response.status_code == 201
+
+# Make a GET request for the entire collection.
+response = requests.get(url, headers=headers)
+assert response.status_code == 200
+print response.json())
+
+# Make a GET request for an individual instance of the model.
+response = requests.get(url + '/1', headers=headers)
+assert response.status_code == 200
+print(response.json())
