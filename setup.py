12ddf387afdb9fd8dc6e732e724f3e66fe0e0de6
Jeffrey Finkelstein <jeffrey.finkelstein@gmail.com>
2014-08-26 16:33:41 -0400

    Bump version number to 0.14.1

--- setup.py
+++ setup.py
@@ -51,6 +51,6 @@ setup(
     test_suite='nose.collector',
     tests_require=['nose'],
     url='http://github.com/jfinkels/flask-restless',
-    version='0.14.1-dev',
+    version='0.14.1',
     zip_safe=False
 )
