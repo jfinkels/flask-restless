c4a25b590ad3cb9f7c9b926e9178e77e9dc9470f
Jeffrey Finkelstein <jeffrey.finkelstein@gmail.com>
2014-10-30 01:57:05 -0400

    Set development version number to 0.15.1-dev

--- flask_restless/__init__.py
+++ flask_restless/__init__.py
@@ -16,7 +16,7 @@
 #:
 #: This should be the same as the version specified in the :file:`setup.py`
 #: file.
-__version__ = '0.15.0'
+__version__ = '0.15.1-dev'
 
 # make the following names available as part of the public API
 from .manager import APIManager
