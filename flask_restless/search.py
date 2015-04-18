a4a1ecd719f0c86c4b7978f640119baed49c9ed6
Jeffrey Finkelstein <jeffrey.finkelstein@gmail.com>
2015-04-18 13:26:09 -0400

    Turns static methods into functions.

--- flask_restless/search.py
+++ flask_restless/search.py
@@ -33,8 +33,8 @@ class ComparisonToNull(Exception):
 
 
 def _sub_operator(model, argument, fieldname):
-    """Recursively calls :func:`QueryBuilder._create_operation` when argument
-    is a dictionary of the form specified in :ref:`search`.
+    """Recursively calls :func:`create_operation` when argument is a dictionary
+    of the form specified in :ref:`search`.
 
     This function is for use with the ``has`` and ``any`` search operations.
 
@@ -49,8 +49,7 @@ def _sub_operator(model, argument, fieldname):
     fieldname = argument['name']
     operator = argument['op']
     argument = argument.get('val')
-    return QueryBuilder._create_operation(submodel, fieldname, operator,
-                                          argument)
+    return create_operation(submodel, fieldname, operator, argument)
 
 
 #: The mapping from operator name (as accepted by the search method) to a
@@ -65,8 +64,8 @@ def _sub_operator(model, argument, fieldname):
 #:
 #: For functions that accept three arguments, the second argument may be a
 #: dictionary containing ``'name'``, ``'op'``, and ``'val'`` mappings so that
-#: :func:`QueryBuilder._create_operation` may be applied recursively. For more
-#: information and examples, see :ref:`search`.
+#: :func:`create_operation` may be applied recursively. For more information
+#: and examples, see :ref:`search`.
 #:
 #: Some operations have multiple names. For example, the equality operation can
 #: be described by the strings ``'=='``, ``'eq'``, ``'equals'``, etc.
@@ -224,182 +223,170 @@ class DisjunctionFilter(JunctionFilter):
         return 'or_{0}'.format(tuple(repr(f) for f in self))
 
 
-class QueryBuilder(object):
-    """Provides a static function for building a SQLAlchemy query object based
-    on a :class:`SearchParameters` instance.
+def create_operation(model, fieldname, operator, argument):
+    """Translates an operation described as a string to a valid SQLAlchemy
+    query parameter using a field or relation of the specified model.
 
-    Use the static :meth:`create_query` method to create a SQLAlchemy query on
-    a given model.
+    More specifically, this translates the string representation of an
+    operation, for example ``'gt'``, to an expression corresponding to a
+    SQLAlchemy expression, ``field > argument``. The recognized operators
+    are given by the keys of :data:`OPERATORS`. For more information on
+    recognized search operators, see :ref:`search`.
+
+    If `relation` is not ``None``, the returned search parameter will
+    correspond to a search on the field named `fieldname` on the entity
+    related to `model` whose name, as a string, is `relation`.
+
+    `model` is an instance of a SQLAlchemy declarative model being
+    searched.
+
+    `fieldname` is the name of the field of `model` to which the operation
+    will be applied as part of the search. If `relation` is specified, the
+    operation will be applied to the field with name `fieldname` on the
+    entity related to `model` whose name, as a string, is `relation`.
+
+    `operation` is a string representating the operation which will be
+     executed between the field and the argument received. For example,
+     ``'gt'``, ``'lt'``, ``'like'``, ``'in'`` etc.
+
+    `argument` is the argument to which to apply the `operator`.
+
+    `relation` is the name of the relationship attribute of `model` to
+    which the operation will be applied as part of the search, or ``None``
+    if this function should not use a related entity in the search.
+
+    This function raises the following errors:
+    * :exc:`KeyError` if the `operator` is unknown (that is, not in
+      :data:`OPERATORS`)
+    * :exc:`TypeError` if an incorrect number of arguments are provided for
+      the operation (for example, if `operation` is `'=='` but no
+      `argument` is provided)
+    * :exc:`AttributeError` if no column with name `fieldname` or
+      `relation` exists on `model`
 
     """
+    # raises KeyError if operator not in OPERATORS
+    opfunc = OPERATORS[operator]
+    # In Python 3.0 or later, this should be `inspect.getfullargspec`
+    # because `inspect.getargspec` is deprecated.
+    numargs = len(inspect.getargspec(opfunc).args)
+    # raises AttributeError if `fieldname` does not exist
+    field = getattr(model, fieldname)
+    # each of these will raise a TypeError if the wrong number of argments
+    # is supplied to `opfunc`.
+    if numargs == 1:
+        return opfunc(field)
+    if argument is None:
+        msg = ('To compare a value to NULL, use the is_null/is_not_null '
+               'operators.')
+        raise ComparisonToNull(msg)
+    if numargs == 2:
+        return opfunc(field, argument)
+    return opfunc(field, argument, fieldname)
 
-    @staticmethod
-    def _create_operation(model, fieldname, operator, argument):
-        """Translates an operation described as a string to a valid SQLAlchemy
-        query parameter using a field or relation of the specified model.
 
-        More specifically, this translates the string representation of an
-        operation, for example ``'gt'``, to an expression corresponding to a
-        SQLAlchemy expression, ``field > argument``. The recognized operators
-        are given by the keys of :data:`OPERATORS`. For more information on
-        recognized search operators, see :ref:`search`.
+def create_filter(model, filt):
+    """Returns the operation on `model` specified by the provided filter.
 
-        If `relation` is not ``None``, the returned search parameter will
-        correspond to a search on the field named `fieldname` on the entity
-        related to `model` whose name, as a string, is `relation`.
+    `filt` is an instance of the :class:`Filter` class.
 
-        `model` is an instance of a SQLAlchemy declarative model being
-        searched.
+    Raises one of :exc:`AttributeError`, :exc:`KeyError`, or
+    :exc:`TypeError` if there is a problem creating the query. See the
+    documentation for :func:`create_operation` for more information.
 
-        `fieldname` is the name of the field of `model` to which the operation
-        will be applied as part of the search. If `relation` is specified, the
-        operation will be applied to the field with name `fieldname` on the
-        entity related to `model` whose name, as a string, is `relation`.
+    """
+    # If the filter is not a conjunction or a disjunction, simply proceed
+    # as normal.
+    if not isinstance(filt, JunctionFilter):
+        fname = filt.fieldname
+        val = filt.argument
+        # get the other field to which to compare, if it exists
+        if filt.otherfield:
+            val = getattr(model, filt.otherfield)
+        # for the sake of brevity...
+        return create_operation(model, fname, filt.operator, val)
+    # Otherwise, if this filter is a conjunction or a disjunction, make
+    # sure to apply the appropriate filter operation.
+    if isinstance(filt, ConjunctionFilter):
+        return and_(create_filter(model, f) for f in filt)
+    return or_(create_filter(model, f) for f in filt)
 
-        `operation` is a string representating the operation which will be
-         executed between the field and the argument received. For example,
-         ``'gt'``, ``'lt'``, ``'like'``, ``'in'`` etc.
 
-        `argument` is the argument to which to apply the `operator`.
+def create_query(session, model, filters=None, sort=None, group_by=None,
+                 _ignore_order_by=False):
+    """Builds an SQLAlchemy query instance based on the search parameters
+    present in ``search_params``, an instance of :class:`SearchParameters`.
 
-        `relation` is the name of the relationship attribute of `model` to
-        which the operation will be applied as part of the search, or ``None``
-        if this function should not use a related entity in the search.
+    This method returns a SQLAlchemy query in which all matched instances
+    meet the requirements specified in ``search_params``.
 
-        This function raises the following errors:
-        * :exc:`KeyError` if the `operator` is unknown (that is, not in
-          :data:`OPERATORS`)
-        * :exc:`TypeError` if an incorrect number of arguments are provided for
-          the operation (for example, if `operation` is `'=='` but no
-          `argument` is provided)
-        * :exc:`AttributeError` if no column with name `fieldname` or
-          `relation` exists on `model`
+    `model` is SQLAlchemy declarative model on which to create a query.
 
-        """
-        # raises KeyError if operator not in OPERATORS
-        opfunc = OPERATORS[operator]
-        # In Python 3.0 or later, this should be `inspect.getfullargspec`
-        # because `inspect.getargspec` is deprecated.
-        numargs = len(inspect.getargspec(opfunc).args)
-        # raises AttributeError if `fieldname` does not exist
-        field = getattr(model, fieldname)
-        # each of these will raise a TypeError if the wrong number of argments
-        # is supplied to `opfunc`.
-        if numargs == 1:
-            return opfunc(field)
-        if argument is None:
-            msg = ('To compare a value to NULL, use the is_null/is_not_null '
-                   'operators.')
-            raise ComparisonToNull(msg)
-        if numargs == 2:
-            return opfunc(field, argument)
-        return opfunc(field, argument, fieldname)
+    `search_params` is an instance of :class:`SearchParameters` which
+    specify the filters, order, limit, offset, etc. of the query.
 
-    @staticmethod
-    def _create_filter(model, filt):
-        """Returns the operation on `model` specified by the provided filter.
+    If `_ignore_order_by` is ``True``, no ``order_by`` method will be
+    called on the query, regardless of whether the search parameters
+    indicate that there should be an ``order_by``. (This is used internally
+    by Flask-Restless to work around a limitation in SQLAlchemy.)
 
-        `filt` is an instance of the :class:`Filter` class.
+    Building the query proceeds in this order:
+    1. filtering
+    2. ordering
+    3. grouping
+    3. limiting
+    4. offsetting
 
-        Raises one of :exc:`AttributeError`, :exc:`KeyError`, or
-        :exc:`TypeError` if there is a problem creating the query. See the
-        documentation for :func:`_create_operation` for more information.
+    Raises one of :exc:`AttributeError`, :exc:`KeyError`, or
+    :exc:`TypeError` if there is a problem creating the query. See the
+    documentation for :func:`create_operation` for more information.
 
-        """
-        # If the filter is not a conjunction or a disjunction, simply proceed
-        # as normal.
-        if not isinstance(filt, JunctionFilter):
-            fname = filt.fieldname
-            val = filt.argument
-            # get the other field to which to compare, if it exists
-            if filt.otherfield:
-                val = getattr(model, filt.otherfield)
-            # for the sake of brevity...
-            create_op = QueryBuilder._create_operation
-            return create_op(model, fname, filt.operator, val)
-        # Otherwise, if this filter is a conjunction or a disjunction, make
-        # sure to apply the appropriate filter operation.
-        create_filt = QueryBuilder._create_filter
-        if isinstance(filt, ConjunctionFilter):
-            return and_(create_filt(model, f) for f in filt)
-        return or_(create_filt(model, f) for f in filt)
+    """
+    query = session_query(session, model)
 
-    @staticmethod
-    def create_query(session, model, filters=None, sort=None, group_by=None,
-                     _ignore_order_by=False):
-        """Builds an SQLAlchemy query instance based on the search parameters
-        present in ``search_params``, an instance of :class:`SearchParameters`.
+    # Filter the query.
+    filters = [Filter.from_dictionary(model, f) for f in filters]
+    # This function call may raise an exception.
+    filters = [create_filter(model, f) for f in filters]
+    query = query.filter(*filters)
 
-        This method returns a SQLAlchemy query in which all matched instances
-        meet the requirements specified in ``search_params``.
-
-        `model` is SQLAlchemy declarative model on which to create a query.
-
-        `search_params` is an instance of :class:`SearchParameters` which
-        specify the filters, order, limit, offset, etc. of the query.
-
-        If `_ignore_order_by` is ``True``, no ``order_by`` method will be
-        called on the query, regardless of whether the search parameters
-        indicate that there should be an ``order_by``. (This is used internally
-        by Flask-Restless to work around a limitation in SQLAlchemy.)
-
-        Building the query proceeds in this order:
-        1. filtering
-        2. ordering
-        3. grouping
-        3. limiting
-        4. offsetting
-
-        Raises one of :exc:`AttributeError`, :exc:`KeyError`, or
-        :exc:`TypeError` if there is a problem creating the query. See the
-        documentation for :func:`_create_operation` for more information.
-
-        """
-        query = session_query(session, model)
-
-        # Filter the query.
-        filters = [Filter.from_dictionary(model, f) for f in filters]
-        # This function call may raise an exception.
-        filters = [QueryBuilder._create_filter(model, f) for f in filters]
-        query = query.filter(*filters)
-
-        # Order the query. If no order field is specified, order by primary
-        # key.
-        if not _ignore_order_by:
-            if sort:
-                for (symbol, field_name) in sort:
-                    direction_name = 'asc' if symbol == '+' else 'desc'
-                    if '.' in field_name:
-                        field_name, field_name_in_relation = \
-                            field_name.split('.')
-                        relation_model = get_related_model(model, field_name)
-                        field = getattr(relation_model, field_name_in_relation)
-                        direction = getattr(field, direction_name)
-                        query = query.join(relation_model)
-                        query = query.order_by(direction())
-                    else:
-                        field = getattr(model, field_name)
-                        direction = getattr(field, direction_name)
-                        query = query.order_by(direction())
-            else:
-                pks = primary_key_names(model)
-                pk_order = (getattr(model, field).asc() for field in pks)
-                query = query.order_by(*pk_order)
-
-        # Group the query.
-        if group_by:
-            for field_name in group_by:
+    # Order the query. If no order field is specified, order by primary
+    # key.
+    if not _ignore_order_by:
+        if sort:
+            for (symbol, field_name) in sort:
+                direction_name = 'asc' if symbol == '+' else 'desc'
                 if '.' in field_name:
-                    field_name, field_name_in_relation = field_name.split('.')
+                    field_name, field_name_in_relation = \
+                        field_name.split('.')
                     relation_model = get_related_model(model, field_name)
                     field = getattr(relation_model, field_name_in_relation)
+                    direction = getattr(field, direction_name)
                     query = query.join(relation_model)
-                    query = query.group_by(field)
+                    query = query.order_by(direction())
                 else:
                     field = getattr(model, field_name)
-                    query = query.group_by(field)
+                    direction = getattr(field, direction_name)
+                    query = query.order_by(direction())
+        else:
+            pks = primary_key_names(model)
+            pk_order = (getattr(model, field).asc() for field in pks)
+            query = query.order_by(*pk_order)
 
-        return query
+    # Group the query.
+    if group_by:
+        for field_name in group_by:
+            if '.' in field_name:
+                field_name, field_name_in_relation = field_name.split('.')
+                relation_model = get_related_model(model, field_name)
+                field = getattr(relation_model, field_name_in_relation)
+                query = query.join(relation_model)
+                query = query.group_by(field)
+            else:
+                field = getattr(model, field_name)
+                query = query.group_by(field)
+
+    return query
 
 
 def search(session, model, filters=None, sort=None, group_by=None,
@@ -443,8 +430,8 @@ def search(session, model, filters=None, sort=None, group_by=None,
     # # corresponding value is anything except those values which evaluate to
     # # False (False, 0, the empty string, the empty list, etc.).
     # is_single = search_params.get('single')
-    query = QueryBuilder.create_query(session, model, filters, sort, group_by,
-                                      _ignore_order_by)
+    query = create_query(session, model, filters, sort, group_by,
+                         _ignore_order_by)
     if single:
         # may raise NoResultFound or MultipleResultsFound
         return query.one()
